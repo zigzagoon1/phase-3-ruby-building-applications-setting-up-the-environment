2929f3602febed5fafba50226d95c7086334a857
KellyMark <kmark602@gmail.com>
2023-05-03 17:29:46 -0400

    commit tree 0b8ad2ee2df5f54c1efd1a28ed6e17b4d76f8605

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 # Require statements should go here.
-
+require_relative '../lib/garden.rb'
+require_relative '../lib/plant.rb'
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
